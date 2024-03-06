1fc30e08e6fb5c68272b626ab96e0408e43e568f
tomnyson <tabletkindfire@gmail.com>
2024-03-06 09:54:00 +0700

    add source

---
+++ bai1/script.js
@@ -0,0 +1,18 @@
+// alert("hello world!");
+
+// khai báo biến let or var
+let so1 = 1;
+let so2 = 1.2;
+let ten= "Lê Hồng Sơn";
+var dungsai = true;
+
+console.log("so1", so1);
+console.log("so1", so2);
+console.log("ten", ten);
+console.log("dungsai", dungsai);
+// hằng số || những biến ko đổi giá trị
+// const dtb = 8.5;
+// dtb = 8.5 + 1;
+// console.log("dtb", dtb);
+document.write("<h1>" + ten + "</h1>");
+document.getElementById("ten").innerHTML = ten;
